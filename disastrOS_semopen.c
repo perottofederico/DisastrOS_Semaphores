7c1b50505d27323f75999ca6026b7b3fa361ed70
perottofederico <perottofederico@gmail.com>
2019-03-04 03:28:12 +0100

    SemOpen done, i think. Need to add the new error codes and globals.

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -7,5 +7,51 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semOpen(){
-  // do stuff :)
+	//Get semaphore id and value from "registers"
+	int sem_num = running->syscall_args[0];
+	int sem_val = running->syscall_args[1];
+
+	//Check if a semaphore with the same id exists already
+	Semaphore* sem = SemaphoreList_byId(&semaphores_list, sem_num); //Need to add sempahore list to globals
+
+	if(!sem){
+		//If it is not found, create it
+		printf("[Seamphore not in list. Creating...]\n")
+		sem = Semaphore_alloc(sem_num, sem_val);
+		if(!sem){
+			printf("[Error in creating Semaphore]\n");
+			running->syscall_retvalue = DSOS_ESEMOPEN; //New error code
+			return;
+		}
+		printf("[Semaphore created]\n");
+		//Insert new semaphore in semaphore list
+		List_insert(&semaphores_list, semaphores_list.last, (ListItem*) sem);
+	}
+
+	//Create the descriptor for the semaphore
+	SemDescriptor* sem_desc = SemDescriptor_alloc(running->last_sem_fd, sem, running);
+	if(!sem_desc){
+		running->syscall_retvalue = DSOS_ESEMDESCCREATE; //New error code
+		return;
+	}
+	printf("[Semaphore descriptor created]\n");
+	//Increase the sem fd value for the next call
+	(running->last_sem_fd)++;
+	//Create a pointer to the descriptor above
+	SemDescriptorPtr*  sem_descptr = SemDescriptorPtr_alloc(sem_desc);
+	printf("[Pointer to the semaphore descriptor created\n]");
+
+	//Add the pointer to the descriptor in sem->desscriptors
+	sem_desc->ptr = sem_descptr;
+	List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) sem_descptr);
+	
+	//Add the descriptor to the list of semaphore descriptors of the process 
+	List_insert(&running->sem_descriptors, running->sem_descriptors.last, (ListItem*)sem_desc); 
+
+	//Return the semaphore id 
+	running->syscall_retvalue = sem_num;
 }
+
+
+
+
